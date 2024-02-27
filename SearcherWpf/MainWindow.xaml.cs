7b5e054e2766f9ccd6bc3e7d8bb191fc87a0a21e
Dave Rayment <dave.rayment@gmail.com>
2024-02-27 20:26:46 +0000

    Started using ViewModel for main window.

--- SearcherWpf/MainWindow.xaml.cs
+++ SearcherWpf/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Ookii.Dialogs.Wpf;
+using SearcherWpf.ViewModels;
 
 namespace SearcherWpf
 {
@@ -31,7 +32,7 @@ namespace SearcherWpf
 			InitializeComponent();
 
 			Loaded += MainWindow_Loaded;
-			SearchCancelButton.Click += SearchCancelButton_Click;
+			//SearchCancelButton.Click += SearchCancelButton_Click;
 
 			FilenameFilter.GotFocus += (sender, e) => FilenameFilterWatermark.Visibility = Visibility.Collapsed;
 			FilenameFilter.LostFocus += (sender, e) => SetWatermarkVisibility(FilenameFilter, FilenameFilterWatermark);
@@ -40,140 +41,131 @@ namespace SearcherWpf
 			SearchPrompt.LostFocus += (sender, e) => SetWatermarkVisibility(SearchPrompt, SearchPromptWatermark);
 		}
 
-		private async void SearchCancelButton_Click(object sender, RoutedEventArgs e)
-		{
-			if ((string)SearchCancelButton.Content == "Cancel")
-			{
-				_cts?.Cancel();
-				return;
-			}
+		//private async void SearchCancelButton_Click(object sender, RoutedEventArgs e)
+		//{
+		//	if ((string)SearchCancelButton.Content == "Cancel")
+		//	{
+		//		_cts?.Cancel();
+		//		return;
+		//	}
 
-			TransitionSearchCancelButton();
-			ResultsList.Items.Clear();
-			_cts = new CancellationTokenSource();
+		//	TransitionSearchCancelButton();
+		//	ResultsList.Items.Clear();
+		//	_cts = new CancellationTokenSource();
 
-			try
-			{
-				await foreach (var result in SearchFiles(
-					SearchPrompt.Text.Trim(), _cts.Token))
-				{
-					ResultsList.Items.Add(result);
-				}
+		//	try
+		//	{
+		//		await foreach (var result in SearchFiles(
+		//			SearchPrompt.Text.Trim(), _cts.Token))
+		//		{
+		//			ResultsList.Items.Add(result);
+		//		}
 
-				Status.Content = $"Finished search. Found {ResultsList.Items.Count} files.";
-			}
-			catch (OperationCanceledException)
-			{
-				Status.Content = "Search cancelled.";
-			}
-			finally
-			{
-				_cts.Dispose();
-				SearchCancelButton.Content = "Search";
-				SearchCancelButton.IsEnabled = true;
-			}
-		}
+		//		Status.Content = $"Finished search. Found {ResultsList.Items.Count} files.";
+		//	}
+		//	catch (OperationCanceledException)
+		//	{
+		//		Status.Content = "Search cancelled.";
+		//	}
+		//	finally
+		//	{
+		//		_cts.Dispose();
+		//		SearchCancelButton.Content = "Search";
+		//		SearchCancelButton.IsEnabled = true;
+		//	}
+		//}
 
-        /// <summary>
-        /// Transition the Search button to a Cancel button after a brief delay.
-        /// </summary>
-        private async void TransitionSearchCancelButton()
-		{
-			SearchCancelButton.IsEnabled = false;
-			await Task.Delay(SearchTransitionDelay);
-			if (_cts != null && !_cts.IsCancellationRequested)
-			{
-				SearchCancelButton.Content = "Cancel";
-				SearchCancelButton.IsEnabled = true;
-			}
-		}
+		///// <summary>
+		///// Transition the Search button to a Cancel button after a brief delay.
+		///// </summary>
+		//private async void TransitionSearchCancelButton()
+		//{
+		//	SearchCancelButton.IsEnabled = false;
+		//	await Task.Delay(SearchTransitionDelay);
+		//	if (_cts != null && !_cts.IsCancellationRequested)
+		//	{
+		//		SearchCancelButton.Content = "Cancel";
+		//		SearchCancelButton.IsEnabled = true;
+		//	}
+		//}
 
-        /// <summary>
-        /// Execute the search. Yields <see cref="FileInfo"/>s for files which
-        /// match the filename pattern and which contain the search text.
-        /// </summary>
-        private async IAsyncEnumerable<FileInfo> SearchFiles(string searchText,
+		/// <summary>
+		/// Execute the search. Yields <see cref="FileInfo"/>s for files which
+		/// match the filename pattern and which contain the search text.
+		/// </summary>
+		private async IAsyncEnumerable<FileInfo> SearchFiles(string searchText,
 			[EnumeratorCancellation] CancellationToken cancellationToken)
 		{
-			if (StartFolder.Text.Trim().Length == 0 ||
-                !Directory.Exists(StartFolder.Text.Trim()))
-			{
-				ShowInvalidDirectoryAlert();
-				yield break;
-			}
+			//if (StartFolder.Text.Trim().Length == 0 ||
+			//	!Directory.Exists(StartFolder.Text.Trim()))
+			//{
+			//	ShowInvalidDirectoryAlert();
+			//	yield break;
+			//}
 
-			if (SearchPrompt.Text.Trim().Length == 0)
-			{
-				MessageBox.Show(this, $"Please supply a search string.",
-					"Missing search string", MessageBoxButton.OK,
-					MessageBoxImage.Error);
-				SearchPrompt.Focus();
-				yield break;
-			}
+			//if (SearchPrompt.Text.Trim().Length == 0)
+			//{
+			//	MessageBox.Show(this, $"Please supply a search string.",
+			//		"Missing search string", MessageBoxButton.OK,
+			//		MessageBoxImage.Error);
+			//	SearchPrompt.Focus();
+			//	yield break;
+			//}
 
-			SaveSettings();
-			FileContents.Document.Blocks.Clear();
+			////SaveSettings();
+			//FileContents.Document.Blocks.Clear();
 
-			Regex pattern = CreateFilenameRegex();
-			var startDirInfo = new DirectoryInfo(StartFolder.Text.Trim());
+			////Regex pattern = CreateFilenameRegex();
+			//var startDirInfo = new DirectoryInfo(StartFolder.Text.Trim());
 
-			foreach (var file in startDirInfo.EnumerateFiles("*", new EnumerationOptions
-			{
-				IgnoreInaccessible = true,
-				RecurseSubdirectories = true,
-				AttributesToSkip = FileAttributes.ReparsePoint
-			}))
-			{
-				cancellationToken.ThrowIfCancellationRequested();
+			//foreach (var file in startDirInfo.EnumerateFiles("*", new EnumerationOptions
+			//{
+			//	IgnoreInaccessible = true,
+			//	RecurseSubdirectories = true,
+			//	AttributesToSkip = FileAttributes.ReparsePoint
+			//}))
+			//{
+			//	cancellationToken.ThrowIfCancellationRequested();
 
-				if (pattern.IsMatch(file.Name))
-				{
-					Status.Content = $"Searching {file.FullName}...";
+			//	if (pattern.IsMatch(file.Name))
+			//	{
+			//		Status.Content = $"Searching {file.FullName}...";
 
-					string contents = await File.ReadAllTextAsync(file.FullName,
-						cancellationToken);
+			//		string contents = await File.ReadAllTextAsync(file.FullName,
+			//			cancellationToken);
 
-					if (contents.Contains(searchText, StringComparison.OrdinalIgnoreCase))
-					{
-						yield return file;
-					}
-				}
-			}
+			//		if (contents.Contains(searchText, StringComparison.OrdinalIgnoreCase))
+			//		{
+			//			yield return file;
+			//		}
+			//	}
+			//}
+
+			yield break;
 		}
 
-        /// <summary>
-        /// Convert the user-supplied filename filter text into a regular expression
-        /// for matching against each of the filenames enumerated.
-        /// </summary>
-        /// <returns>The compiled <see cref="Regex"/> pattern for matching filenames.
-        /// </returns>
-        private Regex CreateFilenameRegex() => new(
-			string.Join("|",
-				FilenameFilter.Text.Split(",").Select(filter =>
-					"^" + Regex.Escape(filter.Trim()).Replace("\\*", ".*").Replace("\\?", ".") + "$")),
-				RegexOptions.Compiled);
+		///// <summary>
+		///// Convert the user-supplied filename filter text into a regular expression
+		///// for matching against each of the filenames enumerated.
+		///// </summary>
+		///// <returns>The compiled <see cref="Regex"/> pattern for matching filenames.
+		///// </returns>
+		//private Regex CreateFilenameRegex() => new(
+		//	string.Join("|",
+		//		FilenameFilter.Text.Split(",").Select(filter =>
+		//			"^" + Regex.Escape(filter.Trim()).Replace("\\*", ".*").Replace("\\?", ".") + "$")),
+		//		RegexOptions.Compiled);
 
-        /// <summary>
-        /// Persist field values between user sessions. Run upon a successful search.
-        /// </summary>
-        private void SaveSettings()
+		/// <summary>
+		/// Show directory not found alert.
+		/// </summary>
+		private void ShowInvalidDirectoryAlert()
 		{
-            Settings.Default.StartFolder = StartFolder.Text;
-            Settings.Default.SearchString = SearchPrompt.Text;
-            Settings.Default.FilenameFilter = FilenameFilter.Text;
-            Settings.Default.Save();
-        }
+			MessageBox.Show(this, $"Directory \"{StartFolder.Text}\"not found.",
+				"Directory Not Found", MessageBoxButton.OK, MessageBoxImage.Error);
+		}
 
-        /// <summary>
-        /// Show directory not found alert.
-        /// </summary>
-        private void ShowInvalidDirectoryAlert()
-		{
-            MessageBox.Show(this, $"Directory \"{StartFolder.Text}\"not found.",
-                "Directory Not Found", MessageBoxButton.OK, MessageBoxImage.Error);
-        }
-
-        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+		private void MainWindow_Loaded(object sender, RoutedEventArgs e)
 		{
 			SetInitialState();
 		}
@@ -184,12 +176,12 @@ namespace SearcherWpf
 			{
 				StartFolderPanel.MinHeight = StartFolder.ActualHeight + 1;
 
-                FilenameFilterGrid.MinHeight = FilenameFilter.ActualHeight + 1;
+				FilenameFilterGrid.MinHeight = FilenameFilter.ActualHeight + 1;
 
-                FilenameFilterWatermark.Visibility = string.IsNullOrEmpty(FilenameFilter.Text) ? Visibility.Visible : Visibility.Collapsed;
-                SearchPromptWatermark.Visibility = string.IsNullOrEmpty(SearchPrompt.Text) ? Visibility.Visible : Visibility.Collapsed;
+				FilenameFilterWatermark.Visibility = string.IsNullOrEmpty(FilenameFilter.Text) ? Visibility.Visible : Visibility.Collapsed;
+				SearchPromptWatermark.Visibility = string.IsNullOrEmpty(SearchPrompt.Text) ? Visibility.Visible : Visibility.Collapsed;
 
-                _initialStateSet = true;
+				_initialStateSet = true;
 			}
 		}
 
@@ -201,61 +193,5 @@ namespace SearcherWpf
 					Visibility.Visible : Visibility.Collapsed;
 			}            
 		}
-
-        private void BrowseForStartFolderButton_Click(object sender, RoutedEventArgs e)
-        {
-            VistaFolderBrowserDialog dialog = new();
-
-            var result = dialog.ShowDialog();
-            if (result.HasValue)
-            {
-                if (Directory.Exists(dialog.SelectedPath))
-                {
-                    StartFolder.Text = dialog.SelectedPath;
-                }
-                else
-                {
-                    ShowInvalidDirectoryAlert();
-                }
-            }
-        }
-
-        //public static void AddWatermark(object element, string watermarkText)
-        //{
-        //    if (element is TextBox textBox)
-        //    {
-        //        RemoveWatermark(element); // Ensure no existing watermark adorner
-
-        //        if (textBox.Text.Length == 0)
-        //        {
-        //            var adornerLayer = AdornerLayer.GetAdornerLayer(textBox);
-        //            if (adornerLayer != null)
-        //            {
-        //                var watermarkAdorner = new WatermarkAdorner(textBox, watermarkText);
-        //                adornerLayer.Add(watermarkAdorner);
-        //            }
-        //        }
-        //    }
-        //}
-
-        //public static void RemoveWatermark(object element)
-        //{
-        //    if (element is TextBox textBox)
-        //    {
-        //        var adornerLayer = AdornerLayer.GetAdornerLayer(textBox);
-        //        if (adornerLayer != null)
-        //        {
-        //            var adorners = adornerLayer.GetAdorners(textBox);
-        //            if (adorners != null)
-        //            {
-        //                var watermarkAdorner = adorners.OfType<WatermarkAdorner>().FirstOrDefault();
-        //                if (watermarkAdorner != null)
-        //                {
-        //                    adornerLayer.Remove(watermarkAdorner);
-        //                }
-        //            }
-        //        }
-        //    }
-        //}
-    }
+	}
 }
---
+++ SearcherWpf/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using Ookii.Dialogs.Wpf;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace SearcherWpf.ViewModels
+{
+	public class MainWindowViewModel : ObservableObject
+	{
+		// Properties.
+		private string _startFolder = Settings.Default.StartFolder;
+		private string _searchString = Settings.Default.SearchString;
+		private string _filenameFilter = Settings.Default.FilenameFilter;
+		private ObservableCollection<FileInfo> _searchResults = [];
+		private string _searchButtonContent = "Search";
+		private bool _isSearchEnabled = true;
+		private bool _searchNotYetRun = true;
+		private CancellationTokenSource? _cts;
+
+		public Visibility NoMatchesFoundVisibility => (!_searchNotYetRun && _searchResults.Count == 0) ? Visibility.Visible : Visibility.Hidden;
+
+		public Visibility SearchNotRunVisibility => _searchNotYetRun ? Visibility.Visible : Visibility.Hidden;
+
+		public string StartFolder
+		{
+			get => _startFolder;
+			set => SetProperty(ref _startFolder, value);
+		}
+
+		public string SearchString
+		{
+			get => _searchString;
+			set => SetProperty(ref _searchString, value);
+		}
+
+		public string FilenameFilter
+		{
+			get => _filenameFilter;
+			set => SetProperty(ref _filenameFilter, value);
+		}
+
+		public ObservableCollection<FileInfo> SearchResults
+		{
+			get => _searchResults;
+			set => SetProperty(ref _searchResults, value);
+		}
+
+		public string SearchButtonContent
+		{
+			get => _searchButtonContent;
+			private set => SetProperty(ref _searchButtonContent, value);
+		}
+
+		public bool IsSearchEnabled
+		{
+			get => _isSearchEnabled;
+			private set => SetProperty(ref _isSearchEnabled, value);
+		}
+
+		// Commands.
+		public IRelayCommand SearchCommand { get; }
+		public IRelayCommand BrowseForStartFolderCommand { get; }
+
+		// Events.
+		public event EventHandler<MessageEventArgs> DirectoryNotFoundError = delegate { };
+
+
+		public MainWindowViewModel()
+		{
+			SearchCommand = new RelayCommand(ExecuteSearchOrCancel);
+			BrowseForStartFolderCommand = new RelayCommand(BrowseForStartFolder);
+		}
+
+		private async void ExecuteSearchOrCancel()
+		{
+			if (SearchButtonContent == "Search")
+			{
+				// Disable the button to prevent accidental double-clicks.
+				IsSearchEnabled = false;
+				await Task.Delay(200);
+
+				SearchButtonContent = "Cancel";
+				IsSearchEnabled = true;
+
+				_cts = new CancellationTokenSource();
+
+				try
+				{
+					_searchResults.Clear();
+					await foreach (var result in SearchFiles(_cts.Token))
+					{
+						_searchResults.Add(result);
+					}
+				}
+				catch (OperationCanceledException)
+				{
+
+				}
+				finally
+				{
+					SearchButtonContent = "Search";
+					IsSearchEnabled = true;
+					OnPropertyChanged(nameof(NoMatchesFoundVisibility));
+				}
+			}
+			else if (SearchButtonContent == "Cancel")
+			{
+				_cts?.Cancel();
+				SearchButtonContent = "Search";
+				IsSearchEnabled = true;
+			}
+		}
+
+		/// <summary>
+		/// Execute the search. Yields <see cref="FileInfo"/>s for files which
+		/// match the filename pattern and which contain the search text.
+		/// </summary>
+		private async IAsyncEnumerable<FileInfo> SearchFiles(
+			[EnumeratorCancellation] CancellationToken cancellationToken)
+		{
+			string trimmedStartFolder = _startFolder.Trim();
+			if (trimmedStartFolder.Length == 0 ||
+				!Directory.Exists(trimmedStartFolder))
+			{
+				OnDirectoryNotFound(trimmedStartFolder);
+				yield break;
+			}
+
+			if (SearchString.Trim().Length == 0)
+			{
+				// TODO: search string error event
+				//MessageBox.Show(this, $"Please supply a search string.",
+				//	"Missing search string", MessageBoxButton.OK,
+				//	MessageBoxImage.Error);
+				//SearchPrompt.Focus();
+				yield break;
+			}
+
+			SaveSettings();
+
+			_searchNotYetRun = false;
+			OnPropertyChanged(nameof(SearchNotRunVisibility));
+
+			// TODO: clear document event
+			//FileContents.Document.Blocks.Clear();
+
+			Regex pattern = CreateFilenameRegex();
+			var startDirInfo = new DirectoryInfo(trimmedStartFolder);
+
+			foreach (var file in startDirInfo.EnumerateFiles("*", new EnumerationOptions
+			{
+				IgnoreInaccessible = true,
+				RecurseSubdirectories = true,
+				AttributesToSkip = FileAttributes.ReparsePoint
+			}))
+			{
+				cancellationToken.ThrowIfCancellationRequested();
+
+				if (pattern.IsMatch(file.Name))
+				{
+					// TODO: status changed event
+					//Status.Content = $"Searching {file.FullName}...";
+
+					string contents = await File.ReadAllTextAsync(file.FullName,
+						cancellationToken);
+
+					if (contents.Contains(SearchString.Trim(),
+						StringComparison.OrdinalIgnoreCase))
+					{
+						yield return file;
+					}
+				}
+			}
+		}
+
+		/// <summary>
+		/// Convert the user-supplied filename filter text into a regular expression
+		/// for matching against each of the filenames enumerated.
+		/// </summary>
+		/// <returns>The compiled <see cref="Regex"/> pattern for matching filenames.
+		/// </returns>
+		private Regex CreateFilenameRegex() => new(
+			string.Join("|",
+				FilenameFilter.Split(",").Select(filter =>
+					"^" + Regex.Escape(filter.Trim()).Replace("\\*", ".*").Replace("\\?", ".") + "$")),
+				RegexOptions.Compiled);
+
+		protected virtual void OnDirectoryNotFound(string directory)
+		{
+			DirectoryNotFoundError?.Invoke(this,
+				new MessageEventArgs { Message = $"{directory} not found." });
+		}
+
+		/// <summary>
+		/// Allow the user to browse for a start folder.
+		/// </summary>
+		private void BrowseForStartFolder()
+		{
+			VistaFolderBrowserDialog dialog = new();
+
+			if (dialog.ShowDialog().HasValue)
+			{
+				if (Directory.Exists(dialog.SelectedPath))
+				{
+					StartFolder = dialog.SelectedPath;
+				}
+				else
+				{
+					OnDirectoryNotFound(dialog.SelectedPath);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Persist field values between user sessions. Run upon a successful search.
+		/// </summary>
+		private void SaveSettings()
+		{
+			Settings.Default.StartFolder = _startFolder;
+			Settings.Default.SearchString = _searchString;
+			Settings.Default.FilenameFilter = _filenameFilter;
+			Settings.Default.Save();
+		}
+	}
+
+	public class MessageEventArgs : EventArgs
+	{
+		public string Message { get; set; }
+	}
+}
