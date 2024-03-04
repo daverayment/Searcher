282f5db95884ac00ba79b9462f9c19d9d04b434f
Dave Rayment <dave.rayment@gmail.com>
2024-03-04 01:37:29 +0000

    Restore link to WinUI Core project. A few spaces-to-tabs formatting updates.

--- SearcherWinUI/ViewModels/MainViewModel.cs
+++ SearcherWinUI/ViewModels/MainViewModel.cs
@@ -13,238 +13,238 @@ namespace SearcherWinUI.ViewModels;
 
 public partial class MainViewModel : ObservableRecipient
 {
-    [ObservableProperty]
-    private string _startFolder = "";
+	[ObservableProperty]
+	private string _startFolder = "";
 
-    [ObservableProperty]
-    private string _searchString = "";
+	[ObservableProperty]
+	private string _searchString = "";
 
-    [ObservableProperty]
-    private string _filenameFilter = "";
+	[ObservableProperty]
+	private string _filenameFilter = "";
 
-    [ObservableProperty]
-    private string _searchButtonContent = "";
+	[ObservableProperty]
+	private string _searchButtonContent = "";
 
-    [ObservableProperty]
-    private bool _isSearchButtonEnabled = true;
+	[ObservableProperty]
+	private bool _isSearchButtonEnabled = true;
 
-    [ObservableProperty]
-    private Visibility _searchNotRunVisibility = Visibility.Visible;
+	[ObservableProperty]
+	private Visibility _searchNotRunVisibility = Visibility.Visible;
 
-    [ObservableProperty]
-    private Visibility _noMatchesFoundVisibility = Visibility.Collapsed;
+	[ObservableProperty]
+	private Visibility _noMatchesFoundVisibility = Visibility.Collapsed;
 
-    [ObservableProperty]
-    private ObservableCollection<FileInfo> _searchResults = new();
+	[ObservableProperty]
+	private ObservableCollection<FileInfo> _searchResults = new();
 
-    /// <summary>
-    /// Delay in milliseconds between a click of the Search button and it
-    /// becoming available to press again as a Cancel button.
-    /// </summary>
-    private const int SearchTransitionDelayInMs = 200;
+	/// <summary>
+	/// Delay in milliseconds between a click of the Search button and it
+	/// becoming available to press again as a Cancel button.
+	/// </summary>
+	private const int SearchTransitionDelayInMs = 200;
 
-    private CancellationTokenSource? _cts;
+	private CancellationTokenSource? _cts;
 
-    public event EventHandler<EventArgs> DirectoryNotFoundError = delegate { };
+	public event EventHandler<EventArgs> DirectoryNotFoundError = delegate { };
 
-    public event EventHandler<EventArgs> BlankSearchStringError = delegate { };
+	public event EventHandler<EventArgs> BlankSearchStringError = delegate { };
 
-    public MainViewModel()
-    {
-        LoadSettings();
-        var resourceLoader = new ResourceLoader();
-        SearchButtonContent = resourceLoader.GetString("Main_SearchButton_Search/Text");
-    }
+	public MainViewModel()
+	{
+		LoadSettings();
+		var resourceLoader = new ResourceLoader();
+		SearchButtonContent = resourceLoader.GetString("Main_SearchButton_Search/Text");
+	}
 
-    private void LoadSettings()
-    {
-        var localSettings = ApplicationData.Current.LocalSettings;
-        StartFolder = (string)(localSettings.Values["StartFolder"] ?? "C:\\");
-        FilenameFilter = (string)(localSettings.Values["FilenameFilter"] ?? "");
-        SearchString = (string)(localSettings.Values["SearchString"] ?? "");
-    }
+	private void LoadSettings()
+	{
+		var localSettings = ApplicationData.Current.LocalSettings;
+		StartFolder = (string)(localSettings.Values["StartFolder"] ?? "C:\\");
+		FilenameFilter = (string)(localSettings.Values["FilenameFilter"] ?? "");
+		SearchString = (string)(localSettings.Values["SearchString"] ?? "");
+	}
 
-    /// <summary>
-    /// Allow the user to browse for a start folder.
-    /// </summary>
-    [RelayCommand]
-    private async void BrowseForStartFolder()
-    {
-        var folderPicker = new FolderPicker
-        {
-            SuggestedStartLocation = PickerLocationId.Desktop
-        };
-        folderPicker.FileTypeFilter.Add("*");
+	/// <summary>
+	/// Allow the user to browse for a start folder.
+	/// </summary>
+	[RelayCommand]
+	private async void BrowseForStartFolder()
+	{
+		var folderPicker = new FolderPicker
+		{
+			SuggestedStartLocation = PickerLocationId.Desktop
+		};
+		folderPicker.FileTypeFilter.Add("*");
 
-        var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(App.MainWindow);
-        WinRT.Interop.InitializeWithWindow.Initialize(folderPicker, hwnd);
+		var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(App.MainWindow);
+		WinRT.Interop.InitializeWithWindow.Initialize(folderPicker, hwnd);
 
-        var folder = await folderPicker.PickSingleFolderAsync();
+		var folder = await folderPicker.PickSingleFolderAsync();
 
-        if (folder != null)
-        {
-            if (Directory.Exists(folder.Path))
-            {
-                StartFolder = folder.Path;
-            }
-            else
-            {
-                OnDirectoryNotFound();
-            }
-        }
-    }
+		if (folder != null)
+		{
+			if (Directory.Exists(folder.Path))
+			{
+				StartFolder = folder.Path;
+			}
+			else
+			{
+				OnDirectoryNotFound();
+			}
+		}
+	}
 
-    [RelayCommand]
-    private async void ExecuteSearchOrCancel()
-    {
-        if (IsCancelButtonShown())
-        {
-            // Cancel a running search operation.
-            _cts?.Cancel();
-            return;
-        }
+	[RelayCommand]
+	private async void ExecuteSearchOrCancel()
+	{
+		if (IsCancelButtonShown())
+		{
+			// Cancel a running search operation.
+			_cts?.Cancel();
+			return;
+		}
 
-        _cts = new CancellationTokenSource();
+		_cts = new CancellationTokenSource();
 
-        // Otherwise begin the search by changing the Search button to Cancel.
-        TransitionSearchButton();
+		// Otherwise begin the search by changing the Search button to Cancel.
+		TransitionSearchButton();
 
-        try
-        {
-            NoMatchesFoundVisibility = Visibility.Collapsed;
-            SearchNotRunVisibility = Visibility.Collapsed;
-            SearchResults.Clear();
+		try
+		{
+			NoMatchesFoundVisibility = Visibility.Collapsed;
+			SearchNotRunVisibility = Visibility.Collapsed;
+			SearchResults.Clear();
 
-            await foreach (var result in SearchFiles())
-            {
-                SearchResults.Add(result);
-            }
-        }
-        catch (OperationCanceledException)
-        {
-            // TODO: report cancelled search.
-        }
-        finally
-        {
-            _cts.Dispose();
-            var loader = new ResourceLoader();
-            SearchButtonContent = loader.GetString("Main_SearchButton_Search/Text");
-            IsSearchButtonEnabled = true;
+			await foreach (var result in SearchFiles())
+			{
+				SearchResults.Add(result);
+			}
+		}
+		catch (OperationCanceledException)
+		{
+			// TODO: report cancelled search.
+		}
+		finally
+		{
+			_cts.Dispose();
+			var loader = new ResourceLoader();
+			SearchButtonContent = loader.GetString("Main_SearchButton_Search/Text");
+			IsSearchButtonEnabled = true;
 
-            if (SearchResults.Count == 0)
-            {
-                NoMatchesFoundVisibility = Visibility.Visible;
-            }
-        }
-    }
+			if (SearchResults.Count == 0)
+			{
+				NoMatchesFoundVisibility = Visibility.Visible;
+			}
+		}
+	}
 
-    private async IAsyncEnumerable<FileInfo> SearchFiles()
-    {
-        if (StartFolder == null || StartFolder.Trim().Length == 0 ||
-            !Directory.Exists(StartFolder.Trim()))
-        {
-            OnDirectoryNotFound();
-            yield break;
-        }
+	private async IAsyncEnumerable<FileInfo> SearchFiles()
+	{
+		if (StartFolder == null || StartFolder.Trim().Length == 0 ||
+			!Directory.Exists(StartFolder.Trim()))
+		{
+			OnDirectoryNotFound();
+			yield break;
+		}
 
-        if (SearchString.Trim().Length == 0)
-        {
-            OnBlankSearchString();
-            yield break;
-        }
+		if (SearchString.Trim().Length == 0)
+		{
+			OnBlankSearchString();
+			yield break;
+		}
 
-        if (_cts == null)
-        {
-            // Should never happen.
-            yield break;
-        }
+		if (_cts == null)
+		{
+			// Should never happen.
+			yield break;
+		}
 
-        SaveSettings();
+		SaveSettings();
 
-        // TODO: clear RichTextBox
-        // TODO: ignore non-text files!
-        // TODO: set default regex if no value has been provided
-        var pattern = CreateFilenameRegex();
-        var startDirInfo = new DirectoryInfo(StartFolder.Trim());
+		// TODO: clear RichTextBox
+		// TODO: ignore non-text files!
+		// TODO: set default regex if no value has been provided
+		var pattern = CreateFilenameRegex();
+		var startDirInfo = new DirectoryInfo(StartFolder.Trim());
 
-        foreach (var file in startDirInfo.EnumerateFiles("*",
-            new EnumerationOptions
-        {
-            IgnoreInaccessible = true,
-            RecurseSubdirectories = true,
-            AttributesToSkip = System.IO.FileAttributes.ReparsePoint
-        }))
-        {
-            _cts.Token.ThrowIfCancellationRequested();
+		foreach (var file in startDirInfo.EnumerateFiles("*",
+			new EnumerationOptions
+		{
+			IgnoreInaccessible = true,
+			RecurseSubdirectories = true,
+			AttributesToSkip = System.IO.FileAttributes.ReparsePoint
+		}))
+		{
+			_cts.Token.ThrowIfCancellationRequested();
 
-            if (pattern.IsMatch(file.Name))
-            {
-                // TODO: status update
-                string contents = await File.ReadAllTextAsync(file.FullName,
-                    _cts.Token);
+			if (pattern.IsMatch(file.Name))
+			{
+				// TODO: status update
+				string contents = await File.ReadAllTextAsync(file.FullName,
+					_cts.Token);
 
-                // TODO: type-specific checks, e.g. rtf/doc files
-                if (contents.Contains(SearchString.Trim(),
-                    StringComparison.OrdinalIgnoreCase))
-                {
-                    yield return file;
-                }
-            }
-        }
-    }
+				// TODO: type-specific checks, e.g. rtf/doc files
+				if (contents.Contains(SearchString.Trim(),
+					StringComparison.OrdinalIgnoreCase))
+				{
+					yield return file;
+				}
+			}
+		}
+	}
 
-    /// <summary>
-    /// Transition the Search button to a Cancel button after a brief delay.
-    /// </summary>
-    private async void TransitionSearchButton()
-    {
-        IsSearchButtonEnabled = false;
-        await Task.Delay(SearchTransitionDelayInMs);
-        if (_cts != null && !_cts.IsCancellationRequested)
-        {
-            var loader = new ResourceLoader();
-            SearchButtonContent = loader.GetString("Main_SearchButton_Cancel/Text");
-            IsSearchButtonEnabled = true;
-        }
-    }
+	/// <summary>
+	/// Transition the Search button to a Cancel button after a brief delay.
+	/// </summary>
+	private async void TransitionSearchButton()
+	{
+		IsSearchButtonEnabled = false;
+		await Task.Delay(SearchTransitionDelayInMs);
+		if (_cts != null && !_cts.IsCancellationRequested)
+		{
+			var loader = new ResourceLoader();
+			SearchButtonContent = loader.GetString("Main_SearchButton_Cancel/Text");
+			IsSearchButtonEnabled = true;
+		}
+	}
 
-    private bool IsCancelButtonShown()
-    {
-        var resourceLoader = new ResourceLoader();
-        return SearchButtonContent ==
-            resourceLoader.GetString("Main_SearchButton_Cancel/Text");
-    }
+	private bool IsCancelButtonShown()
+	{
+		var resourceLoader = new ResourceLoader();
+		return SearchButtonContent ==
+			resourceLoader.GetString("Main_SearchButton_Cancel/Text");
+	}
 
-    /// <summary>
-    /// Convert the user-supplied filename filter text into a regular expression
-    /// for matching against each of the filenames enumerated.
-    /// </summary>
-    /// <returns>The compiled <see cref="Regex"/> pattern for matching filenames.
-    /// </returns>
-    private Regex CreateFilenameRegex() =>
-        new(string.Join("|",
-            FilenameFilter.Split(",").Select(filter =>
-                "^" + Regex.Escape(filter.Trim()).Replace("\\*", ".*").Replace("\\?", ".") + "$")),
-            RegexOptions.Compiled);
+	/// <summary>
+	/// Convert the user-supplied filename filter text into a regular expression
+	/// for matching against each of the filenames enumerated.
+	/// </summary>
+	/// <returns>The compiled <see cref="Regex"/> pattern for matching filenames.
+	/// </returns>
+	private Regex CreateFilenameRegex() =>
+		new(string.Join("|",
+			FilenameFilter.Split(",").Select(filter =>
+				"^" + Regex.Escape(filter.Trim()).Replace("\\*", ".*").Replace("\\?", ".") + "$")),
+			RegexOptions.Compiled);
 
-    /// <summary>
-    /// Persist field values between user sessions. Run upon a successful search.
-    /// </summary>
-    private void SaveSettings()
-    {
-        var localSettings = ApplicationData.Current.LocalSettings;
-        localSettings.Values["StartFolder"] = StartFolder;
-        localSettings.Values["FilenameFilter"] = FilenameFilter;
-        localSettings.Values["SearchString"] = SearchString;
-    }
+	/// <summary>
+	/// Persist field values between user sessions. Run upon a successful search.
+	/// </summary>
+	private void SaveSettings()
+	{
+		var localSettings = ApplicationData.Current.LocalSettings;
+		localSettings.Values["StartFolder"] = StartFolder;
+		localSettings.Values["FilenameFilter"] = FilenameFilter;
+		localSettings.Values["SearchString"] = SearchString;
+	}
 
-    protected virtual void OnDirectoryNotFound()
-    {
-        DirectoryNotFoundError?.Invoke(this, EventArgs.Empty);
-    }
+	protected virtual void OnDirectoryNotFound()
+	{
+		DirectoryNotFoundError?.Invoke(this, EventArgs.Empty);
+	}
 
-    protected virtual void OnBlankSearchString()
-    {
-        BlankSearchStringError?.Invoke(this, EventArgs.Empty);
-    }
+	protected virtual void OnBlankSearchString()
+	{
+		BlankSearchStringError?.Invoke(this, EventArgs.Empty);
+	}
 }
--- SearcherWinUI/Views/MainPage.xaml.cs
+++ SearcherWinUI/Views/MainPage.xaml.cs
@@ -1,19 +1,75 @@
-﻿using Microsoft.UI.Xaml.Controls;
-
+﻿using Microsoft.UI.Text;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Documents;
+using Microsoft.UI.Xaml.Media;
 using SearcherWinUI.ViewModels;
 
 namespace SearcherWinUI.Views;
 
 public sealed partial class MainPage : Page
 {
-    public MainViewModel ViewModel
-    {
-        get;
-    }
+	public MainViewModel ViewModel
+	{
+		get;
+	}
 
-    public MainPage()
-    {
-        ViewModel = App.GetService<MainViewModel>();
-        InitializeComponent();
-    }
+	public MainPage()
+	{
+		ViewModel = App.GetService<MainViewModel>();
+		InitializeComponent();
+	}
+
+	private async void ResultsListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+	{
+		if (ResultsListView.SelectedItem is FileInfo selectedFile)
+		{
+			try
+			{
+				FileContents.Document.SetText(TextSetOptions.ApplyRtfDocumentDefaults, string.Empty);
+				string fileContents =
+					await File.ReadAllTextAsync(selectedFile.FullName);
+
+				// Attempt to reset prior selections.
+				//FileContents.Document.Selection.SetRange(0, TextConstants.MaxUnitCount);
+				//FileContents.Document.Selection.CharacterFormat.BackgroundColor =
+				//	Microsoft.UI.Colors.Transparent;
+				//FileContents.Document.Selection.SetRange(0, 0);
+
+				FileContents.Document.SetText(TextSetOptions.None, fileContents);
+				HighlightMatches();
+			}
+			catch (Exception ex)
+			{
+				// TODO:
+			}
+		}
+	}
+
+	private void HighlightMatches()
+	{
+		string fileContents;
+		FileContents.Document.GetText(TextGetOptions.UseObjectText, out fileContents);
+		string searchString = ViewModel.SearchString;
+
+		bool found = false;
+		int index = fileContents.IndexOf(searchString, 0);
+
+		while (index >= 0)
+		{
+			// Highlight the found text.
+			// NB: setting a range from 0 breaks the control!
+			FileContents.Document.Selection.SetRange(index == 0 ? 1 : index, index + searchString.Length);
+			FileContents.Document.Selection.CharacterFormat.BackgroundColor =
+				Windows.UI.Color.FromArgb(255, 128, 128, 0);
+
+			if (!found)
+			{
+				// Scroll to the first instance of the found text.
+				FileContents.Document.Selection.ScrollIntoView(PointOptions.None);
+				found = true;
+			}
+
+			index = fileContents.IndexOf(searchString, index + searchString.Length);
+		}
+	}
 }
